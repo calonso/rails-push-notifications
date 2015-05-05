b55ddaab576f9b3ade2ea431fa9a07819a2e1cbf
Carlos Alonso <carlos.alonso@mydrivesolutions.com>
2015-05-05 23:23:30 +0100

    refactored base apps class

--- lib/rails-push-notifications/apps.rb
+++ lib/rails-push-notifications/apps.rb
@@ -1,2 +1,3 @@
+require 'rails-push-notifications/apps/base_app'
 require 'rails-push-notifications/apps/apns_app'
 require 'rails-push-notifications/apps/gcm_app'
--- lib/rails-push-notifications/apps/apns_app.rb
+++ lib/rails-push-notifications/apps/apns_app.rb
@@ -1,9 +1,7 @@
 module RailsPushNotifications
-  class APNSApp < ActiveRecord::Base
+  class APNSApp < BaseApp
 
     self.table_name = 'rails_push_notifications_apns_apps'
-
-    has_many :notifications, as: :app
 
     validates :apns_dev_cert, presence: true
     validates :apns_prod_cert, presence: true
@@ -11,31 +9,18 @@ module RailsPushNotifications
 
     before_validation { |app| app.sandbox_mode ||= true }
 
-    def push_notifications
-      pending = find_pending
-      to_send = pending.map do |notification|
-        RubyPushNotifications::APNS::APNSNotification.new notification.destinations, notification.data
-      end
-      pusher = build_pusher
-      pusher.push to_send
-      pending.each_with_index do |p, i|
-        p.update_attributes! results: to_send[i].results
-      end
-    end
-
     private
 
     def cert
       sandbox_mode ? apns_dev_cert : apns_prod_cert
     end
 
-    def find_pending
-      notifications.where sent: false
-    end
-
     def build_pusher
       RubyPushNotifications::APNS::APNSPusher.new cert, sandbox_mode
     end
 
+    def notification_type
+      RubyPushNotifications::APNS::APNSNotification
+    end
   end
 end
